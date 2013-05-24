92e44765d9efca196833bef592b618002931c1eb
Alexander Zschach <alex@zschach.net>
2013-05-24 17:49:57 +0200

    Reformatted and partly re-written

--- install-stable.sh
+++ install-stable.sh
@@ -1,151 +1,134 @@
-#!/bin/sh
-#############################################################
-##
-##	The standard sh shell is used
-##	(dash on ubuntu) because it's
-##	the closest to a posix-compliant
-##	shell that ships with ubuntu
-##	by default. 
-##
-#############################################################
+#! /bin/bash
 
-#############################################################
-##
-##	CONFIGURATION OPTIONS
-##
-#############################################################
+# usage:  e.g. ethdev=eth0 rev="5-2-stable" install-gitlab.sh
 
-REQUIRED_PACKAGES="git git-core gcc libxml2-dev libxslt-dev sqlite3 libsqlite3-dev \
-	libcurl4-openssl-dev libreadline6-dev libc6-dev libssl-dev make build-essential \
-	zlib1g-dev libicu-dev redis-server openssh-server python-dev python-pip libyaml-dev \
-	postfix ruby1.9.3"
-	
-CHUSR="sudo -H -u"
+case "$0" in
+  /*) self=$0 ;;
+  *) self=$(pwd)/$0 ;;
+esac
 
-ETH_ADAPTER="eth1"
+selfdir=$( (cd "${self%/?*}" && pwd) )
+[ "$selfdir" ] || exit 1
 
-#############################################################
-##
-##	MAIN
-##
-#############################################################
-
-#----------- Ensure Root Access -----------
-
-if [ "$(id -u)" != "0" ];
-then
-	echo "Must be run as root. Exiting." 
+if [ "${UID}" != 0 ] ; then
+	echo "This script must be run as user root"
 	exit 1
 fi
 
-#----------- Install Prerequisites -----------
+# the ethernet adapter to use
+: ${ethdev:="eth0"}
+: ${gituser:="git"}
+: ${gitlabuser:="gitlab"}
 
-apt-get install -y $REQUIRED_PACKAGES
+# the list of Ubuntu packages to install
+REQUIRED_PACKAGES="git git-core gcc libxml2-dev libxslt-dev sqlite3 \
+  libsqlite3-dev libcurl4-openssl-dev libreadline6-dev libc6-dev \
+  libssl-dev make build-essential zlib1g-dev libicu-dev redis-server \
+  openssh-server python-dev python-pip libyaml-dev postfix ruby1.9.3"
 
-#----------- Add users -----------
+SUDO="sudo -H -u"
+ETH_ADAPTER="eth0"
 
-adduser --system --shell /bin/sh --gecos 'git user' --group \
-	--disabled-password --home /home/git git
-	
-adduser --disabled-login --gecos 'gitlab system user' gitlab
+# install Ubuntu packages
+apt-get install -y "${REQUIRED_PACKAGES}"
 
-#----------- Add users to eachother's groups -----------
+# add needed git users
+adduser --system --shell /bin/bash --gecos 'git user' --group \
+	--disabled-password --home "/home/${gituser}" "${gituser}"
+adduser --disabled-login --gecos 'gitlab system user' "${gitlabuser}"
 
-usermod -a -G git gitlab
-usermod -a -G gitlab git
+# exchange user groups
+usermod -a -G "${gituser}" "${gitlabuser}"
+usermod -a -G "${gitlabuser}" "${gituser}"
 
-#----------- Generate RSA key ------------
+# generate RSA key
+gitlabhome="/home/${gitlabuser}"
+githome="/home/${gituser}"
 
-$CHUSR gitlab ssh-keygen -q -N '' -t rsa -f /home/gitlab/.ssh/id_rsa
+"${SUDO}" "${gitlabuser}" \
+	"ssh-keygen -q -N '' -t rsa -f ${gitlabhome}/.ssh/id_rsa"
+cp "${gitlabhome}/.ssh/id_rsa.pub" "${githome}/gitlab.pub"
+chmod 0444 "${githome}/gitlab.pub"
 
-cp /home/gitlab/.ssh/id_rsa.pub /home/git/gitlab.pub
-chmod 0444 /home/git/gitlab.pub
+# install gitolite
+cd "${githome}"
+"${SUDO}" "${gituser}" mkdir bin
+"${SUDO}" "${gituser}" git clone -b gl-v304 \
+	https://github.com/gitlabhq/gitolite.git gitolite-src
+"${SUDO}" "${gituser}" \
+	sh -c 'echo "PATH=\$PATH:${githome}/bin" >> ${githome}/.profile'
+"${SUDO}" "${gituser}" \
+	sh -c 'echo "export PATH" >> ${githome}/.profile'
+"${SUDO}" "${gituser}" \
+	gitolite-src/install -ln "${githome}/bin"
+"${SUDO}" "${gituser}" \
+	sh -c 'PATH=${githome}/bin:$PATH; gitolite setup -pk ${githome}/gitlab.pub'
 
-#----------- Install and configure Gitolite -----------
+chmod -R g+rwX "${githome}/repositories/"
+chown -R "${gituser}":"${gituser}" "${githome}/repositories/"
 
-# NOTE: This uses the fork of gitolite tweaked for GitLab
-
-cd /home/git
-
-$CHUSR git mkdir bin
-$CHUSR git git clone -b gl-v304 https://github.com/gitlabhq/gitolite.git gitolite-src
-$CHUSR git sh -c 'echo "PATH=\$PATH:/home/git/bin" >> /home/git/.profile'
-$CHUSR git sh -c 'echo "export PATH" >> /home/git/.profile'
-$CHUSR git gitolite-src/install -ln /home/git/bin
-$CHUSR git sh -c 'PATH=/home/git/bin:$PATH; gitolite setup -pk /home/git/gitlab.pub'
-
-chmod -R g+rwX /home/git/repositories/
-chown -R git:git /home/git/repositories/
-
-#----------- Test the Install -------------
-
-$CHUSR gitlab git clone git@localhost:gitolite-admin.git /tmp/gitolite-admin
+# test the install
+"${SUDO}" "${gitlabuser}" \
+	git clone git@localhost:gitolite-admin.git /tmp/gitolite-admin
 rm -rf /tmp/gitolite-admin
 
-#----------- Install GitLab --------------
-
+# install GitLab
 gem install --conservative charlock_holmes --version '0.6.8'
 pip install pygments
 gem install --conservative bundler
-cd /home/gitlab
+cd "${gitlabhome}"
 
-$CHUSR gitlab git clone -b stable https://github.com/gitlabhq/gitlabhq.git gitlab
+# clone GitLab
+"${SUDO}" "${gitlabuser}" \
+	git clone -b stable https://github.com/gitlabhq/gitlabhq.git gitlab
 cd gitlab
 
-$CHUSR gitlab cp config/gitlab.yml.example config/gitlab.yml
+# config GitLab
+"${SUDO}" "${gitlabuser}" \
+	cp config/gitlab.yml.example config/gitlab.yml
 
-#--- Using SQLite
+# use SQLite (TODO: ??? really  ???)
+"${SUDO}" "${gitlabuser}" \
+	cp config/database.yml.sqlite config/database.yml
 
-$CHUSR gitlab cp config/database.yml.sqlite config/database.yml
+# install gems for database
+"${SUDO}" "${gitlabuser}" \
+	bundle install --without development test mysql postgres --deployment
 
-#--- Install gems for database 
+# setup the db
+"${SUDO}" "${gitlabuser}" \
+	bundle exec rake gitlab:app:setup RAILS_ENV=production
 
-$CHUSR gitlab bundle install --without development test mysql postgres --deployment
+# setup hooks
+cp ./lib/hooks/post-receive \
+	"${githome}/.gitolite/hooks/common/post-receive"
+chown "${gituser}":"${gituser}" \
+	"${githome}/.gitolite/hooks/common/post-receive"
 
-#--- Setup database
+# check status
+echo "" ; echo "Checking install status ..." ; echo ""
+"${SUDO}" "${gitlabuser}" \
+	bundle exec rake gitlab:app:status RAILS_ENV=production
 
-$CHUSR gitlab bundle exec rake gitlab:app:setup RAILS_ENV=production
-
-#--- Setup hooks
-
-cp ./lib/hooks/post-receive /home/git/.gitolite/hooks/common/post-receive
-chown git:git /home/git/.gitolite/hooks/common/post-receive
-
-#--- Check status
-
-echo ""
-echo "CHECKING INSTALL STATUS..."
-echo ""
-
-$CHUSR gitlab bundle exec rake gitlab:app:status RAILS_ENV=production
-
-echo ""
-echo "WERE ALL ANSWERS ABOVE = YES ?"
-echo ""
-
-read ANSWER
-echo "Answer: $ANSWER"
-if [ "$ANSWER" != "yes" ];
-then
-	echo "Received answer other than 'yes'. Exiting"
+echo "" ; echo "Everything passed? Please type 'yes' or 'no'" ; echo ""
+read GO_ON
+if [ "${GO_ON}" != "yes" ] ; then
+	echo "Your answer was not 'yes'.  Exiting."
 	exit 1
 fi
 
-echo ""
-echo "Succeeded."
-echo "Testing the installation..."
-
-#------------ Test GitLab ------------
-
-cd /home/gitlab/gitlab
-$CHUSR gitlab bundle exec rails s -e production
+# test GitLab
+echo "" ; echo "Succeeded." ; echo "Testing the installation"
+cd "${gitlabhome}/gitlab"
+"${SUDO}" "${gitlabuser}" bundle exec rails s -e production
 
 echo ""
-IP_ADDR=$(ifconfig $ETH_ADAPTER | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
+IP_ADDR=$(ifconfig ${ethdev} | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1 }')
 echo "Visit: $IP_ADDR:3000 from your browser, and login with:"
 echo "admin@local.host	5iveL!fe"
 echo ""
-echo "NOTE: It will take a while to load the page the first time it is accessed, due to compiling times on java-script, jquery core, and css files."
+echo "NOTE: It will take a while to load the page the first time it is accessed,"
+echo "due to compiling times on java-script, jquery core, and css files."
 echo ""
 
 
