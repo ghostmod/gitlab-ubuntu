7904b8a0a47a1b06a10b49e1b83d57bd6e9f67d5
Alexander Zschach <alex@zschach.net>
2013-05-25 02:27:54 +0200

    Finalized first shot ... Not yet finished, but we are damn near :-)

--- install-gitlab.sh
+++ install-gitlab.sh
@@ -30,10 +30,18 @@ fi
 : ${gituser:="git"}
 : ${rev:="5-2-stable"}
 
-: ${mysqluser:="root"}
-: ${mysqlpass:="GhuP3412,bv"}
+: ${mysqluser:="gitlab"}
+: ${mysqlpass:="gitlab,"}
+: ${mysql_rootpass:="GhuP3412,bv"}
 
 githome="/home/${gituser}"
+
+# Generate RSA key
+
+# ${SUDO} "${gituser}" \
+# 	"ssh-keygen -q -N '' -t rsa -f ${githome}/.ssh/id_rsa"
+# cp "${githome}/.ssh/id_rsa.pub" "${githome}/gitlab.pub"
+# chmod 0444 "${githome}/gitlab.pub"
 
 # the list of Ubuntu packages to install
 REQUIRED_PACKAGES="build-essential checkinstall curl gcc git git-core \
@@ -41,7 +49,7 @@ REQUIRED_PACKAGES="build-essential checkinstall curl gcc git git-core \
 	libncurses5-dev libreadline-dev libreadline6-dev libsqlite3-dev \
 	libssl-dev libxml2-dev libxslt-dev libyaml-dev make openssh-server \
 	postfix python-dev python-pip redis-server ruby1.9.3 sqlite3 sudo \
-	vim zlib1g-dev"
+	vim zlib1g-dev nginx"
 
 # Shall we install MySQL server?  If yes, configure it automatically
 # FIXME:  let user choose if he wants PostgreSQL instead?
@@ -50,25 +58,34 @@ install_mysql=$(echo "${install_mysql}" | tr "[:lower:]" "[:upper:]")
 if [[ -z ${install_mysql} || ${install_mysql} == "Y" ]]; then
 	REQUIRED_PACKAGES+=" mysql-common mysql-client mysql-server libmysqlclient-dev"
 	# prepare for non-interactive configuration
-	echo "mysql-server-5.5 mysql-server/root_password password ${mysqlpass}" | \
+	echo "mysql-server-5.5 mysql-server/root_password password ${mysql_rootpass}" | \
 		debconf-set-selections
-	echo "mysql-server-5.5 mysql-server/root_password_again password ${mysqlpass}" | \
+	echo "mysql-server-5.5 mysql-server/root_password_again password ${mysql_rootpass}" | \
 		debconf-set-selections
 fi
 
+# TODO:  Request information for Postfix installation
+
 SUDO="sudo -H -u"
 
-# install Ubuntu packages
+# Install Ubuntu packages
+
 echo "Installing required Ubuntu packages ... This might take a minute!"
 ( echo "apt-get update" && \
 	apt-get update > /dev/null 2>&1 && \
 	apt-get install -y ${REQUIRED_PACKAGES}
 ) || echo "installation of required Ubuntu packages failed"
 
-# add needed git users
+# Install bundler
+gem install --conservative bundler
+pip install pygments
+
+# Add git system user
+
 adduser --disabled-login --gecos 'GitLab' "${gituser}"
 
-# install GitLab shell
+# Install GitLab shell
+
 cd "${githome}"
 ${SUDO} "${gituser}" git clone https://github.com/gitlabhq/gitlab-shell.git
 ${SUDO} "${gituser}" test -d gitlab-shell && cd gitlab-shell
@@ -78,16 +95,11 @@ ${SUDO} "${gituser}" chmod 0755 ./bin/install && ./bin/install
 
 # MySQL setup
 
-# ###################################################################
-# TODO!!!!
-# mysql -u root -p"${mysqlpass}" "CREATE USER 'gitlab'@'localhost' IDENTIFIED BY ${mysqlpass}"
-# mysql -u root -p \
-# 	"CREATE DATABASE IF NOT EXISTS `gitlabhq_production` DEFAULT CHARACTER SET `utf8` COLLATE `utf8_unicode_ci`"
-# mysql -u root -p \
-# 	"GRANT SELECT, LOCK TABLES, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER ON `gitlabhq_production`.* TO 'gitlab'@'localhost'"
-# # Try connecting to the new database with the new user
-# sudo -u git -H mysql -u gitlab -p -D gitlabhq_production
-# ####################################################################
+if [[ -z ${install_mysql} || ${install_mysql} == "Y" ]]; then
+	mysql -u root -p"${mysql_rootpass}" < create.sql
+fi
+
+# Get GitLab from github
 
 cd "${githome}"
 ${SUDO} "${gituser}" \
@@ -97,17 +109,22 @@ ${SUDO} "${gituser}" git checkout "${rev}"
 
 # Configure GitLab
 
-cd "${githome}"
+cd "${githome}/gitlab"
 ${SUDO} "${gituser}" cp config/gitlab.yml.example config/gitlab.yml
+# FIXME
 #${SUDO} "${gituser}" vim config/gitlab.yml ## TODO
 
-mkdirs="${githome}/gitlab-satellites log tmp tmp/pids tmp/sockets public/uploads"
+mkdirs="${githome}/gitlab-satellites log tmp tmp/pids"
+mkdirs="${mkdirs} tmp/sockets public/uploads"
+
 for dir in ${mkdirs} ; do
 	${SUDO} "${gituser}" mkdir -p "${dir}" && \
 		${SUDO} "${gituser}" chmod -R u+rwX "${dir}"
 done
 
 # Copy the example Puma config
+
+# FIXME: auto configuration
 ${SUDO} "${gituser}" cp config/puma.rb.example config/puma.rb
 
 # Configure Git global settings for git user, useful when editing via web
@@ -115,90 +132,90 @@ ${SUDO} "${gituser}" cp config/puma.rb.example config/puma.rb
 ${SUDO} "${gituser}" git config --global user.name "GitLab"
 ${SUDO} "${gituser}" git config --global user.email "gitlab@localhost"
 
-exit 1 # TODO
+# Setup GitLab for usage of MySQL (as preferred)
 
-# generate RSA key
+${SUDO} "${gituser}" cp config/database.yml.mysql config/database.yml
 
-${SUDO} "${gitlabuser}" \
-	"ssh-keygen -q -N '' -t rsa -f ${gitlabhome}/.ssh/id_rsa"
-cp "${gitlabhome}/.ssh/id_rsa.pub" "${githome}/gitlab.pub"
-chmod 0444 "${githome}/gitlab.pub"
+# Install Gems
 
-# install gitolite
+charlock_holmes_ver='0.6.0.4'
 cd "${githome}"
-${SUDO} "${gituser}" mkdir bin
-${SUDO} "${gituser}" git clone -b gl-v304 \
-	https://github.com/gitlabhq/gitolite.git gitolite-src
-${SUDO} "${gituser}" \
-	sh -c 'echo "PATH=\$PATH:${githome}/bin" >> ${githome}/.profile'
-${SUDO} "${gituser}" \
-	sh -c 'echo "export PATH" >> ${githome}/.profile'
-${SUDO} "${gituser}" \
-	gitolite-src/install -ln "${githome}/bin"
-${SUDO} "${gituser}" \
-	sh -c 'PATH=${githome}/bin:$PATH; gitolite setup -pk ${githome}/gitlab.pub'
+gem install charlock_holmes --version "${charlock_holmes_ver}"
 
-chmod -R g+rwX "${githome}/repositories/"
+# fix wrong file encoding!
+f="${githome}/gitlab/vendor/bundle/ruby/1.9.1/specifications/gitlab-grit-2.5.1.gemspec"
+iconv -f iso-8859-1 -t utf8 "${f}" "${f}_utf8"
+rm "${f}"
+mv "${f}_utf8" "${f}"
+
+# fix utf8 typo!
+cat "${f}" | sed s/s.add_runtime_depeîdency/s.add_runtime_dependency/g > "${f}_x"
+rm "${f}"
+mv "${f}_x" "${f}"
+
+cd "${githome}/gitlab"
+${SUDO} "${gituser}" bundle install --deployment --without development test postgres
+
+# Initialize db
+
+##### STOP HERE, CONFIGURE database.yml to match root PW!
+
+echo "Set root password to ${mysql_rootpass} in ~git/gitlab/config/database.yml "
+echo "and press return when done"
+echo "" ; read
+
 chown -R "${gituser}":"${gituser}" "${githome}/repositories/"
+cd "${githome}/gitlab"
+${SUDO} "${gituser}" bundle exec rake gitlab:setup RAILS_ENV=production
+${SUDO} "${gituser}" bundle exec rake sidekiq:start RAILS_ENV=production
 
-# test the install
-${SUDO} "${gitlabuser}" \
-	git clone git@localhost:gitolite-admin.git /tmp/gitolite-admin
-rm -rf /tmp/gitolite-admin
+# Install Init Script
 
-# install GitLab
-gem install --conservative charlock_holmes --version '0.6.8'
-pip install pygments
-gem install --conservative bundler
-cd "${gitlabhome}"
+curl \
+	--output "/etc/init.d/gitlab" \
+		"https://raw.github.com/gitlabhq/gitlabhq/5-2-stable/lib/support/init.d/gitlab"
+chmod +x /etc/init.d/gitlab
 
-# clone GitLab
-${SUDO} "${gitlabuser}" \
-	git clone -b "${rev}" https://github.com/gitlabhq/gitlabhq.git gitlab
-cd gitlab
+# TODO: make configurable
+update-rc.d gitlab defaults 21
 
-# config GitLab
-${SUDO} "${gitlabuser}" \
-	cp config/gitlab.yml.example config/gitlab.yml
+# Configure Nginx
 
-# use SQLite (TODO: ??? really  ???)
-${SUDO} "${gitlabuser}" \
-	cp config/database.yml.sqlite config/database.yml
+curl \
+	--output "/etc/nginx/sites-available/gitlab" \
+		"https://raw.github.com/gitlabhq/gitlabhq/5-2-stable/lib/support/nginx/gitlab"
+ln -s "/etc/nginx/sites-available/gitlab" "/etc/nginx/sites-enabled/gitlab"
 
-# install gems for database
-${SUDO} "${gitlabuser}" \
-	bundle install --without development test mysql postgres --deployment
+# TODO!
 
-# setup the db
-${SUDO} "${gitlabuser}" \
-	bundle exec rake gitlab:app:setup RAILS_ENV=production
+# # **YOUR_SERVER_FQDN** to the fully-qualified
+# # domain name of your host serving GitLab. Also, replace
+# # the 'listen' line with the following:
+# #   listen 80 default_server;         # e.g., listen 192.168.1.1:80;
+# sudo vim /etc/nginx/sites-available/gitlab
 
-# setup hooks
-cp ./lib/hooks/post-receive \
-	"${githome}/.gitolite/hooks/common/post-receive"
-chown "${gituser}":"${gituser}" \
-	"${githome}/.gitolite/hooks/common/post-receive"
+# configure NGINX manually for the moment!
 
-# check status
-echo "" ; echo "Checking install status ..." ; echo ""
-${SUDO} "${gitlabuser}" \
-	bundle exec rake gitlab:app:status RAILS_ENV=production
+sudo service nginx restart
 
-echo "" ; echo "Everything passed? Please type 'yes' or 'no'" ; echo ""
-read GO_ON
-if [ "${GO_ON}" != "yes" ] ; then
+# Final testing
+
+${SUDO} "${gituser}" bundle exec rake gitlab:env:info RAILS_ENV=production
+${SUDO} "${gituser}" bundle exec rake gitlab:check RAILS_ENV=production
+
+echo "" ; echo "Everything passed? [Y|n] " ; echo "" ; read passed
+passed=$(echo "${passed}" | tr "[:lower:]" "[:upper:]")
+if [ "${passed}" != "Y" ] ; then
 	echo "Your answer was not 'yes'.  Exiting."
 	exit 1
 fi
 
-# test GitLab
-echo "" ; echo "Succeeded." ; echo "Testing the installation"
-cd "${gitlabhome}/gitlab"
-${SUDO} "${gitlabuser}" bundle exec rails s -e production
+echo "Starting GitLab service"
+service gitlab start
 
 echo ""
 IP_ADDR=$(ifconfig ${ethdev} | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1 }')
-echo "Visit: $IP_ADDR:3000 from your browser, and login with:"
+echo "Visit: $IP_ADDR from your browser, and login with:"
 echo "admin@local.host	5iveL!fe"
 echo ""
 echo "NOTE: It will take a while to load the page the first time it is accessed,"
